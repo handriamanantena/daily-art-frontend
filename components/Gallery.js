ea1865de5a122e16f3b2e2f2d4930df40cb13ff2
harrison <harrison.a101@hotmail.com>
2021-08-30 00:36:13 -0400

    added css styling to restrict to 4 pictures per row on large screens and 3 pictures in smaller screens

--- components/Gallery.js
+++ components/Gallery.js
@@ -6,10 +6,12 @@ class Gallery extends React.Component {
     constructor(props) {
         super(props);
        // this.state = {
-        this.photo  = ["https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=802&q=80",
-                "https://images.unsplash.com/photo-1518568740560-333139a27e72?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
+        this.photo  = [            "https://mcdn.wallpapersafari.com/medium/45/24/WR46bc.jpg",
+                "https://cdn.crispedge.com/f1bd89.png",
             "https://images.unsplash.com/photo-1518568740560-333139a27e72?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
-            "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+            "https://images.unsplash.com/photo-1518568740560-333139a27e72?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
+            "https://images.unsplash.com/photo-1518568740560-333139a27e72?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
+            "https://images.unsplash.com/photo-1518568740560-333139a27e72?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
             "https://images.unsplash.com/photo-1518568740560-333139a27e72?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
             "https://images.unsplash.com/photo-1518568740560-333139a27e72?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
             "https://images.unsplash.com/photo-1518568740560-333139a27e72?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
@@ -35,14 +37,17 @@ n
     generateFrames() {
         for(let i =0; i < this.photo; i++) {
             console.log('I was triggered during componentDidMount');
-            return (
-                <div className={styles.grid}>
-                <a className={styles.card}>
-                    <img src={this.photo[i]}/>
+            return <div className={[gallery.frame]} key={index} >
+                <a className={gallery.card}>
+                    <img className={gallery.imageFrame} src={this.photo[index]}/>
                 </a>
-            </div> );
+            </div>;
         }
     }
+}
+
+class Frame extends React.Component {
+
 }
 
 export default Gallery;
