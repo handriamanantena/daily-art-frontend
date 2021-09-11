00512b561694f3cf192cf04a825ab00e297d9e4f
harrison <harrison.a101@hotmail.com>
2021-09-11 00:51:03 -0400

    infinite scroll by calling api

--- pages/dailyart.js
+++ pages/dailyart.js
@@ -1,48 +1,43 @@
 import Gallery from "../components/Gallery";
-/*import { promises as fs } from 'fs'
-import path from 'path';*/
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import gallery from "../styles/Gallery.module.css";
 
 
 function DailyArt({ galleries }) {
-   React.useEffect(() => {
-      const handleScroll = e => {
-         console.log('scroll')
-         if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-            console.log('bottom')
-            console.log('bottom')
-            console.log('bottom')
-            let newGallery = galleries[0]
-            newGallery.page = 1
-           galleries.push(newGallery)
 
-
-         }
-      }
-      document.addEventListener("scroll", handleScroll, { passive: true })
+   let [galleryList, setGalleryList] = useState(galleries)
+   useEffect(() => {
+      window.addEventListener("scroll", handleScroll)
       return () => {
-         // This cleans up the event handler when the component unmounts.
-         document.removeEventListener("scroll", handleScroll)
+         window.removeEventListener("scroll", handleScroll)
       }
-   }, [])
-  /* const [gallery, setUsers] = useState([])*/
+   })
+   let isLoading = false
 
+   const handleScroll =  async () => {
+      if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight && !isLoading) {
+         isLoading = true
+         getNextGallery().then(async (value) => {
+            galleries.push(value)
+            setGalleryList([... galleries])
+            isLoading = false
+         })
+      }
+   }
    return (<div className={ gallery.gallery}>
             {
-                (galleries.map((gallery, i) => {
-                  return <Gallery pictures = {gallery.pictures} key = {i}/>
+                (galleryList.map((gallery, i) => {
+                  return <Gallery pictures = {gallery.pictures} key = {gallery.page}/>
                }))
             }
          </div>);
 
 }
 
-async function getNextGallery(galleries) {
+async function getNextGallery() {
    const host = 'http://192.168.0.130:3001'
    const res = await fetch(host + "/pictures?date=2021-10");
-   const gallery = await res.json()
-   return galleries.push(await gallery)
+   return await res.json()
 }
 
 
@@ -59,31 +54,6 @@ export async function getStaticProps() {
       }
    }
 }
-
-
-/*export async function getStaticProps() {
-   const postsDirectory = "F:\\art\\pictures\\test"
-   const filenames = await fs.readdir(postsDirectory)
-   const root = 'http://192.168.0.130:3001/file/'
-   const pictures = filenames.map(async (filename) => {
-      const filePath = path.join(postsDirectory, filename)
-      const fileContents = await fs.readFile(filePath, 'utf8')
-
-      // Generally you would parse/transform the contents
-      // For example you can transform markdown to HTML here
-
-      return {
-         url: root + filename
-      }
-   })
-   // By returning { props: { pictures } }, the DailyArt component
-   // will receive `pictures` as a prop at build time
-   return {
-      props: {
-         pictures: await Promise.all(pictures),
-      },
-   }
-}*/
 
 
 export default DailyArt;
