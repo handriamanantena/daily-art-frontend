b77f4e87c4a1ac5f35465c1c9569e64fcedae249
harrison <harrison.a101@hotmail.com>
2021-09-19 13:27:48 -0400

    moved picture to separate component

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,5 +1,5 @@
 import gallery from '../styles/Gallery.module.css'
-import React from 'react';
+import React, {useState} from 'react';
 import Image from 'next/image'
 
 
@@ -9,28 +9,31 @@ class Gallery extends React.Component {
     }
 
     render() {
-        let host = 'http://192.168.0.130:3001/file/'
         return this.props.pictures.map((value, index) => {
-                    let url = encodeURI(host + value.url)
                     return <div className={[gallery.frame]} key={value._id} >
-                        <a className={gallery.card} key={value._id}>
-                            <Image className={gallery.imageFrame}
-                                   width={1035}
-                                   height={1228}
-                                   src={url}
-                                   key={value._id}
-                                   quality={100}/>
-                            <p className={ gallery.pictureName}>{value.pictureName}</p>
-                        </a>
+                        <ShowPictures picture={value}> </ShowPictures>
                     </div>
                 })
 
         ;
     }
-}
 
-class Frame extends React.Component {
 
 }
+
+function ShowPictures({picture}) {
+    let host = 'http://192.168.0.130:3001/file/'
+    let url = encodeURI(host + picture.url)
+    return  (<a className={gallery.card} key={picture._id}>
+        <Image className={gallery.imageFrame}
+               width={1035}
+               height={1228}
+               src={url}
+               key={picture._id}
+               quality={100}/>
+        <p className={ gallery.pictureName}>{picture.pictureName}</p>
+    </a>);
+}
+
 
 export default Gallery;
