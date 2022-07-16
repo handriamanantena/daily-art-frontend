80efccd9be314104a30af0ef3f9287b7469e5e65
harrison <harrison.a101@hotmail.com>
2022-07-16 18:18:32 -0400

    update tailwind to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
