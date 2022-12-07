44bebe22b7ab307fedeedc1d8adf8d965fca300f
harrison <harrison.a101@hotmail.com>
2022-12-07 17:57:39 -0500

    style credential inputs on focus

--- components/input/CredentialInputs.js
+++ components/input/CredentialInputs.js
@@ -1,13 +1,14 @@
-import {Fragment} from "react";
-import React, { useState } from 'react';
 
 export default function CredentialInput({type, id, name, onKeyDown, passwordStrength}) {
 
-    return (<Fragment>
+
+
+    return (<div tabindex="0" className="flex mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 focus:outline-none
+        focus-within:border-sky-500 focus-within:ring-sky-500 focus-within:ring-1
+        block w-full rounded-md sm:text-sm ">
         <input type={type} id={id} name={name} required onInput={onKeyDown}
-               className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300
-               focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"/>
-        <span>{passwordStrength}</span>
-    </Fragment>);
+               className="flex-1 bg-transparent focus:outline-none"/>
+        <span className="bg-transparent focus:outline-none">{passwordStrength}</span>
+    </div>);
 
 }
