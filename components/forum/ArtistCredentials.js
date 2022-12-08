f3be37b7980e2ca5334c91d4179dedda86602c96
harrison <harrison.a101@hotmail.com>
2022-12-08 01:53:36 -0500

    changed login button style

--- components/forum/ArtistCredentials.js
+++ components/forum/ArtistCredentials.js
@@ -7,15 +7,13 @@ export default function ArtistCredentials({welcomeTitle, artistInfoTitle, artist
         <form onSubmit={onSubmit} className="grid grid-cols-1 place-content-cente w-[440px] p-10">
             <h2 className="font-extrabold">{welcomeTitle}</h2>
             <label htmlFor="userName" className="mt-10 mb-1">{artistInfoTitle}</label>
-            <CredentialInput type={artistInfoInputType} id="userName" name="userName" required onKeyDown={onKeyDown}></CredentialInput>
+            <CredentialInput type={artistInfoInputType} id="userName" name="userName" required/>
             <span className="h-5 flex"/>
             <label htmlFor="password" className="mb-1">{artistPasswordTitle}</label>
-            <div>
-                <CredentialInput type="password" id="password" name="password" required onKeyDown={onKeyDown} passwordStrength={passwordStrength}></CredentialInput>
-            </div>
-            <label className="text-gray-400 text-xs">{passwordFlavourText}</label>
+            <CredentialInput type="password" id="password" name="password" required onKeyDown={onKeyDown} passwordStrength={passwordStrength}/>
+            <label className="text-gray-400 text-xs mb-5">{passwordFlavourText}</label>
 
-            <button type="submit">Log In</button>
+            <button type="submit" className="h-10 bg-blue-500 text-white hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500">Log In</button>
         </form>
     </div>);
 }
