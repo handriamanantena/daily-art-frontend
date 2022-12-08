da260d644491385f07241c68a14a36330c42ddc2
harrison <harrison.a101@hotmail.com>
2022-12-08 14:00:36 -0500

    added divider between adding credentials and multiple login

--- components/forum/ArtistCredentials.js
+++ components/forum/ArtistCredentials.js
@@ -1,9 +1,10 @@
 import CredentialInput from "../input/CredentialInputs";
+import {Fragment} from "react";
 
 export default function ArtistCredentials({welcomeTitle, artistInfoTitle, artistPasswordTitle,
                                               passwordFlavourText, onSubmit, passwordStrength, onKeyDown, artistInfoInputType}) {
 
-    return (<div className="flex-col shadow-lg">
+    return (<Fragment>
         <form onSubmit={onSubmit} className="grid grid-cols-1 place-content-cente w-[440px] p-10">
             <h2 className="font-extrabold">{welcomeTitle}</h2>
             <label htmlFor="userName" className="mt-10 mb-1">{artistInfoTitle}</label>
@@ -12,8 +13,12 @@ export default function ArtistCredentials({welcomeTitle, artistInfoTitle, artist
             <label htmlFor="password" className="mb-1">{artistPasswordTitle}</label>
             <CredentialInput type="password" id="password" name="password" required onKeyDown={onKeyDown} passwordStrength={passwordStrength}/>
             <label className="text-gray-400 text-xs mb-5">{passwordFlavourText}</label>
-
             <button type="submit" className="h-10 bg-blue-500 text-white hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500">Log In</button>
+            <div className="flex items-center mt-2 mb-2">
+                <div className="grow border-b border-stone-400"/>
+                <div className="ml-2 mr-2">or</div>
+                <div className="grow border-b border-stone-400"/>
+            </div>
         </form>
-    </div>);
+    </Fragment>);
 }
