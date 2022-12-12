ab1fa09128687fcfeea38333d0ddea96bcf45290
harrison <harrison.a101@hotmail.com>
2022-12-12 14:43:29 -0500

    changed daily art to Today art

--- components/button/NavigationImageLink.js
+++ components/button/NavigationImageLink.js
@@ -6,7 +6,7 @@ export function NavigationImageLink({path, imagePath, text}) {
     return (<Link href={path}>
         <a className="px-5 flex">
             { imagePath ? <Image src={imagePath} width={24} height={24}/> : <></> }
-            { text ? <h1>{text}</h1> : <></> }
+            { text ? <h3 className="font-bold">{text}</h3> : <></> }
         </a>
     </Link>)
 }
--- components/common/NavigationBar.js
+++ components/common/NavigationBar.js
@@ -6,13 +6,14 @@ import {NavigationImageLink} from "../button/NavigationImageLink";
 
 export function NavigationBar() {
 
-    return (<header className="flex pl-5">
+    return (<header className="flex pl-5 items-center">
         <button className="flex-none pt-2">
             <Image src="/icons/bars-solid.svg" width={24} height={24}/>
         </button>
         <NavigationImageLink path="/" imagePath="/icons/pen-to-square-regular.svg" text="Daily Art"/>
         <div className="bg-slate-600 w-px h-5"></div>
-            <NavigationImageLink path="/" text="Daily Art"/>
+            <NavigationImageLink path="/" text="Today's Art"/>
+        <div className="bg-slate-600 w-px h-5"></div>
         <div className="flex ml-auto">
             <button>
                 Sign In
