70e7923dc115145d58703ac7fe0a043cbd91af78
harrison <harrison.a101@hotmail.com>
2022-12-17 12:09:56 -0500

    moved auto login from nav bar to basiclayout

--- components/common/BasicLayout.js
+++ components/common/BasicLayout.js
@@ -1,8 +1,56 @@
-import {Fragment} from "react";
+import {Fragment, useContext, useEffect} from "react";
 import {NavigationBar} from "./NavigationBar"
 import {Footer} from "./Footer"
+import AuthContext from "../../common/context/auth-context";
+import useAxiosPrivate from "../../common/hooks/useAxiosPrivate";
 
 export function BasicLayout({children}) {
+
+    const ctx = useContext(AuthContext);
+    const axiosPrivate = useAxiosPrivate();
+
+    useEffect(async () => {
+        console.log("testing login")
+        // API endpoint where we send form data.
+        let endpoint = process.env.REACT_APP_PICTURES_API_HOST + process.env.REACT_APP_PICTURES_API_PORT + "/refresh";
+        if(ctx.token == undefined) {
+            console.log("refresh");
+            try {
+                const response = await axiosPrivate.post(endpoint,
+                    {},
+                    {
+                        headers: {'Content-Type': 'application/json'},
+                        withCredentials: true
+                    }
+                );
+                console.log(JSON.stringify(response?.data));
+                const accessToken = response?.data?.accessToken;
+                ctx.login(accessToken);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        else {
+            console.log("login test")
+            endpoint = process.env.REACT_APP_PICTURES_API_HOST + process.env.REACT_APP_PICTURES_API_PORT + "/login"
+
+            try {
+                const response = await axiosPrivate.post(endpoint,
+                    {},
+                    {
+                        headers: {'Content-Type': 'application/json',
+                            'Authorization': 'Bearer ' + ctx.token},
+                        withCredentials: true
+                    }
+                );
+                console.log(JSON.stringify(response?.data));
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+
+    }, [ctx.token])
 
     return (<Fragment>
         <NavigationBar/>
--- components/common/NavigationBar.js
+++ components/common/NavigationBar.js
@@ -3,57 +3,14 @@ import Image from "next/image";
 import {NavigationImageLink} from "../button/NavigationImageLink";
 import AuthContext from "../../common/context/auth-context";
 import {useRouter} from "next/router";
-import useAxiosPrivate from "../../common/hooks/useAxiosPrivate";
 
 
 export function NavigationBar() {
 
     const router = useRouter();
-    const axiosPrivate = useAxiosPrivate();
     const ctx = useContext(AuthContext);
 
-    useEffect(async () => {
-        console.log("testing login")
-        // API endpoint where we send form data.
-        let endpoint = process.env.REACT_APP_PICTURES_API_HOST + process.env.REACT_APP_PICTURES_API_PORT + "/refresh";
-        if(ctx.token == undefined) {
-            console.log("refresh");
-            try {
-                const response = await axiosPrivate.post(endpoint,
-                    {},
-                    {
-                        headers: {'Content-Type': 'application/json'},
-                        withCredentials: true
-                    }
-                );
-                console.log(JSON.stringify(response?.data));
-                const accessToken = response?.data?.accessToken;
-                ctx.login(accessToken);
-            } catch (err) {
-              console.log(err);
-            }
-        }
-        else {
-            console.log("login test")
-            endpoint = process.env.REACT_APP_PICTURES_API_HOST + process.env.REACT_APP_PICTURES_API_PORT + "/login"
 
-            try {
-                const response = await axiosPrivate.post(endpoint,
-                    {},
-                    {
-                        headers: {'Content-Type': 'application/json',
-                                    'Authorization': 'Bearer ' + ctx.token},
-                        withCredentials: true
-                    }
-                );
-                console.log(JSON.stringify(response?.data));
-            } catch (err) {
-                console.log(err);
-            }
-        }
-
-
-    }, [ctx.token])
     const logout = () => {
       ctx.logout();
     }
