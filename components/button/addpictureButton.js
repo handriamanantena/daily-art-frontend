145939035bbd7c8097dce4608558f04a261dc5b3
harrison <harrison.a101@hotmail.com>
2023-05-31 14:24:37 -0400

    add picture button only shows up on log in. however there is a bug. it always shows up. it should only show up for the loged in users account

--- components/button/addpictureButton.js
+++ components/button/addpictureButton.js
@@ -1,9 +1,14 @@
+import {Fragment, useContext} from "react";
+import AuthContext from "../../common/context/auth-context";
 
 
 export const AddPictureButton = () => {
 
-
+    const ctx = useContext(AuthContext);
     return (
-            <button className="flex flex-col w-1/2 sm:w-3/10 lg:w-1/4 border-4 border-yellow-500 text-9xl justify-center items-center text-yellow-500 rounded-lg" title="Add Picture">+</button>
+        <Fragment>
+        {ctx.isLoggedIn ? <button className="flex flex-col w-1/2 sm:w-3/10 lg:w-1/4 border-4 border-yellow-500 text-9xl justify-center items-center text-yellow-500 rounded-lg" title="Add Picture">+</button> :
+            <Fragment></Fragment>}
+        </Fragment>
     );
 }
