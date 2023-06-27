695c2016c8d52a9712721e0c6ad0ecb487a7d988
harrison <harrison.a101@hotmail.com>
2023-06-27 19:53:09 -0400

    image loader returns blank

--- next.config.js
+++ next.config.js
@@ -6,13 +6,14 @@ module.exports = {
         REACT_APP_PICTURES_API_PORT:'',
         //REACT_API_PICTURES_API_IP: '192.168.0.108',
         REACT_API_PICTURES_API_IP: 'https://api.dailyirasuto.com',
-        REACT_APP_CDN_IMAGES: 'https://dailyartworker.harrison-a101.workers.dev/',
+        REACT_APP_CDN_IMAGES: 'https://dailyirasuto.com/',
     },
     images: {
         //domains: ['192.168.0.108', 'localhost']
-        domains: ['https://dailyartworker.harrison-a101.workers.dev', 'localhost'],
+        unoptimized: true,
+        domains: ['https://dailyirasuto.com', 'localhost'],
         loader: 'custom',
-        loaderFile: './common/image/imageLoader.js',
+        loaderFile: './common/image/imageLoader.js'
     },
     async redirects() {
         return [
