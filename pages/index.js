05ad57a53fb89abe2628ecb1e4f6fb4d5f96d602
harrison <harrison.a101@hotmail.com>
2023-06-27 23:14:08 -0400

    Updated home page

--- pages/index.js
+++ pages/index.js
@@ -2,30 +2,30 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import React from "react";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Daily Art</title>
         <meta name="description" content="Daily Art" />
         <link rel="icon" href="/icons/pen-to-square-regular.svg" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         Simple-Art
+            Simple Art
         </h1>
-
-        <p className={styles.description}>
-          A drawing a day
+          <p className={styles.description}>
+              A drawing a day
         </p>
 
         <div className={styles.grid}>
           <Link href="/dailyart">
             <a className={styles.card}>
-                <h2>Daily Art &rarr;</h2>
-                <p>Previous Simple-Art available on the marketplace.</p>
+                <h2>イラスト &rarr;</h2>
+                <p>An illustration a day keeps the tears away</p>
             </a>
           </Link>
         </div>
