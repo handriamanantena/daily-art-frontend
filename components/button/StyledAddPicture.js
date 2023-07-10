57d976e8b6373738c7327b73c33240733bd0bf9d
harrison <harrison.a101@hotmail.com>
2023-07-10 14:58:45 -0400

    changed size of add button

--- components/button/StyledAddPicture.js
+++ components/button/StyledAddPicture.js
@@ -1,6 +1,6 @@
 
 export const StyledAddPicture = ({text, showPopUp}) => {
 
-    return(<button onClick={showPopUp} className="flex flex-col w-1/2 sm:w-3/10 lg:w-1/4 border-4 border-yellow-500 text-9xl justify-center items-center text-yellow-500 rounded-lg" title="Add Picture">
+    return(<button onClick={showPopUp} className="flex flex-col w-1/2 h-96 sm:w-3/10 lg:w-1/4 border-4 border-yellow-500 text-9xl justify-center items-center text-yellow-500 rounded-lg" title="Add Picture">
         {text}</button>);
 };
