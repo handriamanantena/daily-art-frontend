5d8122e3f602c4685f27bff9885975706ef25235
harrison <harrison.a101@hotmail.com>
2023-07-18 18:06:41 -0400

    test fallback true

--- pages/dailyart.js
+++ pages/dailyart.js
@@ -1,20 +1,26 @@
 import Gallery from "../components/Gallery";
 import React, {useEffect, useState, useRef, Fragment} from 'react';
 import dailyArt from '../styles/DailyArt.module.css'
-import {getNextGallery, getPicturesByArtistUserName, getPicturesByPage} from "../common/api/pictures";
+import {getPicturesByPage, getPicturesByPageClientSide} from "../common/api/pictures";
 import {BasicLayout} from "../components/common/BasicLayout";
 import {InfiniteScroll} from "../components/InfiniteScroll";
 import Loading from "../components/loading/loading";
-import {unstable_scheduleCallback as delay} from "scheduler";
 
-let pageSize = 10;
+let pageSize = 2;
 
 
-function DailyArt({ pictures }) {
-    let [newPictures, setPictures] = useState(pictures)
-    let [isLoading, setIsLoading] = useState(false)
+function DailyArt() {
+    let [pictures, setPictures] = useState([]);
+    let [isLoading, setIsLoading] = useState(false);
     let [lastElement, setLastElement] = useState(null);
-    let [pageIndex, setPageIndex] = useState(pictures[pictures.length - 1]._id);
+    let [pageIndex, setPageIndex] = useState(undefined);
+
+    useEffect(async () => {
+        let response = await getPicturesByPage(null, pageSize, null);
+        setPictures(response);
+        setPageIndex(response[response.length-1]._id);
+
+    }, []);
 
     let getPictures = async () => {
         setIsLoading(true)
@@ -31,7 +37,7 @@ function DailyArt({ pictures }) {
        <BasicLayout>
           <h1 className={dailyArt.simpleArtTitle}>Simple Art</h1>
           <InfiniteScroll getObjects = {getPictures} maxPage = {10} lastElement={lastElement}>
-             <Gallery pictures = {newPictures} setLastElement = {setLastElement}/>
+             <Gallery pictures = {pictures} setLastElement = {setLastElement}/>
               { isLoading ? <Loading/> : <Fragment></Fragment>}
           </InfiniteScroll>
        </BasicLayout>);
@@ -39,14 +45,14 @@ function DailyArt({ pictures }) {
 }
 
 
-export async function getStaticProps() {
+/*export async function getStaticProps() {
    const pictures =  await getPicturesByPage(null, pageSize, null);
    return {
       props: {
          pictures : pictures
       }
    }
-}
+}*/
 
 
 export default DailyArt;
