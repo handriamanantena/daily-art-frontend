764f833b5bcfaae5433f94bc545ecb75120264a2
harrison <harrison.a101@hotmail.com>
2023-08-22 13:31:49 -0400

    changed intro text for daily words popup

--- components/popup/DrawingOfTheDay.js
+++ components/popup/DrawingOfTheDay.js
@@ -18,8 +18,9 @@ export const DrawingOftheDay = () => {
     }, []);
 
 
-    return <div className="grid md:min-w-[24rem] px-10 pt-10 pb-10 min-h-[25rem] max-h-screen max-w-fit md:max-w-none">
-        <h1 className="justify-self-center">Today we are drawing...</h1>
+    return <div className="grid md:min-w-[24rem] px-10 pt-10 pb-10 max-h-screen max-w-fit md:max-w-none">
+        <h1 className="justify-self-center">Daily drawing challenge...</h1>
+        <h1 className="justify-self-center text-6xl py-5">🎨</h1>
         <h1 className="justify-self-center text-slate-400">{word.japanese}/{word.english}</h1>
     </div>
 }
