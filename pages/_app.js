3c1c44d79d0d45548c739284ed40a47236ca6087
harrison <harrison.a101@hotmail.com>
2023-08-29 17:55:40 -0400

    removed script, not needed for next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,10 @@
 import 'dotenv/config';
 import '../styles/globals.css'
 import {AuthProvider} from "../common/context/auth-context";
-import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return <AuthProvider>
       <Component {...pageProps} />
-      <Script
-          type="text/javascript"
-          src="../node_modules/tw-elements/dist/js/tw-elements.umd.min.js"/>
   </AuthProvider>
 }
 export default MyApp
