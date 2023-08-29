b301edde84d26e78ac4a97da123dc5145b746992
harrison <harrison.a101@hotmail.com>
2023-08-29 14:25:31 -0400

    Removed Link tag from slide menu items

--- components/button/SlideMenuImageLink.js
+++ components/button/SlideMenuImageLink.js
@@ -1,16 +1,16 @@
-import Image from "next/image";
-import Link from "next/link";
 import {H3NavHideOnMobile} from "../common/H3NavHideOnMobile";
 import React from "react";
 
-export function SlideMenuImageLink({path, as, imagePath, text}) {
+export function SlideMenuImageLink({path, text, children}) {
 
-    return (<Link href={path} as={as}>
-        <a className="flex h-12 cursor-pointer items-center truncate rounded-[5px] px-6 py-4 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10">
-            { imagePath ? <div className="flex items-center justify-center">
-                <Image src={imagePath} width={20} height={20} unoptimized/>
-            </div>: <></> }
-            { text ? <H3NavHideOnMobile text={text}/>: <></> }
-        </a>
-    </Link>)
+    return (<a className="group flex h-12 cursor-pointer items-center truncate px-6 py-4 text-[0.875rem]
+     text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-300 hover:text-inherit
+      hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit
+       active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none
+        motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10" href={path}>
+            <div className="flex items-center justify-center pr-5">
+                {children}
+            </div>
+            <H3NavHideOnMobile text={text}/>
+    </a>)
 }
--- components/common/HeaderBar.js
+++ components/common/HeaderBar.js
@@ -108,13 +108,25 @@ export function HeaderBar() {
                  data-te-sidenav-backdropClass="bg-black">
                 <ul>
                     <li className="relative">
-                        <SlideMenuImageLink path="/dailyart" imagePath="/icons/house-solid.svg" text="Home"/>
+                        <SlideMenuImageLink path="/dailyart" text="Home">
+                            <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} className="group-hover:fill-cyan-600" viewBox="0 0 576 512">
+                                <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/>
+                            </svg>
+                        </SlideMenuImageLink>
                     </li>
                     <li className="relative">
-                    <SlideMenuImageLink path="/" imagePath="/icons/rocket-solid.svg" text="Challenges"/>
+                        <SlideMenuImageLink path="/" text="Challenges">
+                            <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} className="group-hover:fill-cyan-600" viewBox="0 0 512 512">
+                                <path d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2v82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9V380.8c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/>
+                            </svg>
+                        </SlideMenuImageLink>
                     </li>
                     <li className="relative">
-                        <SlideMenuImageLink path="/" imagePath="/icons/trophy-solid.svg" text="Leader board"/>
+                        <SlideMenuImageLink path="/" text="Leader board">
+                            <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} className="group-hover:fill-cyan-600"  viewBox="0 0 576 512">
+                                <path d="M400 0H176c-26.5 0-48.1 21.8-47.1 48.2c.2 5.3 .4 10.6 .7 15.8H24C10.7 64 0 74.7 0 88c0 92.6 33.5 157 78.5 200.7c44.3 43.1 98.3 64.8 138.1 75.8c23.4 6.5 39.4 26 39.4 45.6c0 20.9-17 37.9-37.9 37.9H192c-17.7 0-32 14.3-32 32s14.3 32 32 32H384c17.7 0 32-14.3 32-32s-14.3-32-32-32H357.9C337 448 320 431 320 410.1c0-19.6 15.9-39.2 39.4-45.6c39.9-11 93.9-32.7 138.2-75.8C542.5 245 576 180.6 576 88c0-13.3-10.7-24-24-24H446.4c.3-5.2 .5-10.4 .7-15.8C448.1 21.8 426.5 0 400 0zM48.9 112h84.4c9.1 90.1 29.2 150.3 51.9 190.6c-24.9-11-50.8-26.5-73.2-48.3c-32-31.1-58-76-63-142.3zM464.1 254.3c-22.4 21.8-48.3 37.3-73.2 48.3c22.7-40.3 42.8-100.5 51.9-190.6h84.4c-5.1 66.3-31.1 111.2-63 142.3z"/>
+                            </svg>
+                        </SlideMenuImageLink>
                     </li>
                 </ul>
             </div>
