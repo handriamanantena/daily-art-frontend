c5c6e8585998341cc8a967c53b34f11a116f57e9
harrison <harrison.a101@hotmail.com>
2023-09-01 14:44:21 -0400

    edit about works

--- components/forum/inputs/TextArea.js
+++ components/forum/inputs/TextArea.js
@@ -1,10 +1,10 @@
 import {TextAreaBorder} from "./TextAreaBorder";
 
-export const TextArea = ({defaultValue}) => {
+export const TextArea = ({id, name, defaultValue}) => {
 
     return <TextAreaBorder>
-        <textarea className="md:min-h-[174px] p-2.5 w-full bg-transparent focus:outline-none"
-                     defaultValue={defaultValue} maxLength={5000}></textarea>
+        <textarea id={id} name={name} className="md:min-h-[174px] p-2.5 w-full bg-transparent focus:outline-none"
+                  defaultValue={defaultValue} maxLength={5000}/>
     </TextAreaBorder>
 
 }
--- components/popup/EditProfile.js
+++ components/popup/EditProfile.js
@@ -1,22 +1,45 @@
 import BasicForumInput from "../forum/inputs/input";
 import SubmitButton from "../forum/inputs/SubmitButton";
-import React from "react";
+import React, {useContext} from "react";
 import {PopupForm} from "./PopupForm";
 import {TextArea} from "../forum/inputs/TextArea";
+import AuthContext from "../../common/context/auth-context";
 
 export const EditProfile = ({userInfo}) => {
 
+    let ctx = useContext(AuthContext);
 
-    let onSubmit = () => {
 
+    let onSubmit = async (e) => {
+        e.preventDefault();
+        let data = JSON.stringify({
+                about: event.target.about.value,
+                userName: event.target.userName.value
+            });
+        const host = process.env.NEXT_PUBLIC_PICTURES_API_HOST + process.env.NEXT_PUBLIC_PICTURES_API_PORT;
+        let path = host + `/artist/${ctx.email}`;
+        let response = await fetch(path, {
+            method: 'PATCH',
+            credentials: 'include', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + ctx.token
+            },
+            body : data
+        });
+        if(response.status == 201) {
+            setTimeout(() => {
+                window.location.reload();
+            }, +(process.env.NEXT_PUBLIC_REVALIDATE_SEC) * 1000);
+        }
     };
 
     return <PopupForm onSubmit={onSubmit}>
         <h1>Edit Profile</h1>
         <label htmlFor="userName">User name</label>
-        <BasicForumInput type="text" id="userName" name="userName" defaultValue={userInfo.userName}/>
+        <BasicForumInput type="text" id="userName" name="userName" maxLength={32} defaultValue={userInfo.userName}/>
         <label htmlFor="about">About</label>
-        <TextArea defaultValue={userInfo.about}/>
+        <TextArea id="about" name="about" defaultValue={userInfo.about}/>
         <SubmitButton text="Submit"/>
     </PopupForm>
 
