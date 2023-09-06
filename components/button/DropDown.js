205ce6a3ad94c3c05ffcf3b829cc3e2c88b8f929
harrison <harrison.a101@hotmail.com>
2023-09-06 16:50:53 -0400

    click on dropdown now closes dropdown

--- components/button/DropDown.js
+++ components/button/DropDown.js
@@ -10,16 +10,13 @@ export const DropDown = ({children, menuOption, hideDropDownArrow, id}) => {
         }
 
         function handleAnyDropDownClick(event) {
-
-            if(event.target.id != `dropDown${id}`) {
+            if (!ref.current.contains(event.target)) {
                 setShowDropDown(false);
             }
         }
-        // Bind the event listener
         document.addEventListener("click", handleClickOutside, false);
         document.addEventListener("click", handleAnyDropDownClick, {capture: true});
         return () => {
-            // Unbind the event listener on clean up
             document.removeEventListener("click", handleClickOutside, false);
             document.removeEventListener("click", handleAnyDropDownClick, {capture: true});
         };
@@ -32,7 +29,7 @@ export const DropDown = ({children, menuOption, hideDropDownArrow, id}) => {
     };
 
     return <div className="group flex" ref={ref} id={`dropDown${id}`}>
-        <div onClick={dropDown} className="flex z-10">
+        <button onClick={dropDown} className="flex z-10">
         {menuOption}
             <div className={`grid content-center pl-1 ${hideDropDownArrow ? 'hidden' : 'block'}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" height="0.9em" viewBox="0 0 448 512"
@@ -41,7 +38,7 @@ export const DropDown = ({children, menuOption, hideDropDownArrow, id}) => {
                         d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/>
                 </svg>
             </div>
-        </div>
+        </button>
         {showDropDown ? children : <></>}
     </div>
 }
