11a419bbe8cc4c853d47505820fbe748ab989a6c
harrison <harrison.a101@hotmail.com>
2023-09-25 16:02:10 -0400

    added split to date function

--- common/Utility.js
+++ common/Utility.js
@@ -2,13 +2,9 @@ import Moment from "moment";
 
 
 export const formatDateJapanease = (date) => {
-    return Moment(date).format('YYYY年 MMM月 D日');
+    return Moment(date.split('T')[0]).format('YYYY年 MMM月 D日');
 }
 
 export const formatDateYYYYMMDD = (date) => {
-    console.log("previouse date: " + date);
-    console.log("moment day: " + Moment(date).date());
-    let formated = Moment(date).format("YYYY-MM-DD")
-    console.log("formated date" + formated);
-    return formated
+    return Moment(new Date(date).toISOString().split('T')[0]).format("YYYY-MM-DD");
 }
